3c7a30833c72abb834420e85f1008d85819b04e6
braunixy <braunsmich@yahoo.de>
2017-06-30 13:47:42 +0200

    !

--- src/main/java/server/askboard/group/myserveraskboard/controller/QuestionController.java
+++ src/main/java/server/askboard/group/myserveraskboard/controller/QuestionController.java
@@ -1,10 +1,13 @@
 package server.askboard.group.myserveraskboard.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import server.askboard.group.myserveraskboard.entities.Answer;
 import server.askboard.group.myserveraskboard.entities.Question;
+import server.askboard.group.myserveraskboard.security.UserCustomDetails;
 import server.askboard.group.myserveraskboard.services.QuestionService;
+import server.askboard.group.myserveraskboard.services.UserService;
 
 import java.util.List;
 
@@ -14,6 +17,8 @@ public class QuestionController {
 
     @Autowired
     private QuestionService questionService;
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     private String index() {
@@ -42,13 +47,24 @@ public class QuestionController {
 
     @RequestMapping(value = "/new", method = RequestMethod.POST)
     public Question askQuestion(@RequestBody Question question) {
+        UserCustomDetails details =
+                (UserCustomDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        question.setOwner(details.getUsername());
         questionService.insert(question);
+        userService.findByUsername(details.getUsername()).getQuestions().add(question);
         return question;
     }
 
     @RequestMapping(value = "/answer/{id}", method = RequestMethod.POST)
     public Question answerQuestion(@PathVariable("id") Long id, @RequestBody Answer answer) {
-        return questionService.answerQuestionById(id, answer);
+        UserCustomDetails details =
+                (UserCustomDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        answer.setOwner(details.getUsername());
+        Question question = questionService.answerQuestionById(id, answer);
+        userService.findByUsername(details.getUsername()).getAnswers().add(answer);
+        return question;
     }
 
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
